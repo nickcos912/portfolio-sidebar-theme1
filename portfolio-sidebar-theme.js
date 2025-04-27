e45aed70853463ed81af29dd6f518e3aca2df216
nickcos912 <npc5452@psu.edu>
2025-04-27 14:21:43 -0400

    put code in wrong js file

--- portfolio-sidebar-theme.js
+++ portfolio-sidebar-theme.js
@@ -8,12 +8,9 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
 /**
  * `portfolio-sidebar-theme`
- * 
- * @demo index.html
  * @element portfolio-sidebar-theme
  */
 export class PortfolioSidebarTheme extends DDDSuper(I18NMixin(LitElement)) {
-
   static get tag() {
     return "portfolio-sidebar-theme";
   }
@@ -28,14 +25,11 @@ export class PortfolioSidebarTheme extends DDDSuper(I18NMixin(LitElement)) {
     };
     this.registerLocalization({
       context: this,
-      localesPath:
-        new URL("./locales/portfolio-sidebar-theme.ar.json", import.meta.url).href +
-        "/../",
+      localesPath: new URL("./locales/portfolio-sidebar-theme.ar.json", import.meta.url).href + "/../",
       locales: ["ar", "es", "hi", "zh"],
     });
   }
 
-  // Lit reactive properties
   static get properties() {
     return {
       ...super.properties,
@@ -43,42 +37,40 @@ export class PortfolioSidebarTheme extends DDDSuper(I18NMixin(LitElement)) {
     };
   }
 
-  // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--portfolio-sidebar-theme-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
+    return [
+      super.styles,
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        .wrapper {
+          margin: 0;
+          padding: var(--ddd-spacing-4);
+          display: flex;
+          min-height: 100vh;
+        }
+        h3 span {
+          font-size: var(--portfolio-sidebar-theme-label-font-size, var(--ddd-font-size-s));
+        }
+      `,
+    ];
   }
 
-  // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+      <div class="wrapper">
+        ${this.title ? html`<h3><span>${this.t.title}:</span> ${this.title}</h3>` : ''}
+        <slot></slot>
+      </div>
+    `;
   }
 
-  /**
-   * haxProperties integration via file reference
-   */
   static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url).href;
   }
 }
-
 globalThis.customElements.define(PortfolioSidebarTheme.tag, PortfolioSidebarTheme);
